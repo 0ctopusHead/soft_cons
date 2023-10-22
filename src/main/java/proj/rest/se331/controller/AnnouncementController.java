aebac82922e806e5b72ba31979f0f913582a68b7
0ctopusHead <yanwarut_s@cmu.ac.th>
2023-10-23 04:17:20 +0700

    edit get announcement

--- src/main/java/proj/rest/se331/controller/AnnouncementController.java
+++ src/main/java/proj/rest/se331/controller/AnnouncementController.java
@@ -3,6 +3,7 @@ package proj.rest.se331.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import proj.rest.se331.entity.Advisor;
 import proj.rest.se331.entity.Files;
+import proj.rest.se331.entity.Student;
 import proj.rest.se331.request.AnnouncementRequest;
 import proj.rest.se331.service.AdvisorService;
 import proj.rest.se331.service.AnnouncementService;
+import proj.rest.se331.service.StudentService;
 import proj.rest.se331.util.LabMapper;
 
 import java.util.List;
@@ -21,12 +24,14 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 public class AnnouncementController {
+    final StudentService studentService;
     final AnnouncementService announcementService;
     final AdvisorService advisorService;
 
     @GetMapping("/announcement/{id}")
     public ResponseEntity<?> getAnnouncement(@PathVariable("id")Long id){
-        Advisor advisor = advisorService.getAdvisor(id);
+        Advisor id_advisor = studentService.getStudent(id).getAdvisor();
+        Advisor advisor = advisorService.getAdvisor(id_advisor.getId());
         List<Files> output = announcementService.getFiles(advisor.getId());
         if(output != null){
             return ResponseEntity.ok(LabMapper.INSTANCE.getFileDTO(output));
